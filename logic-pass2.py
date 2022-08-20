9611f06341858c3fa39d2a43f21c1fb77ba720be
hajersatar <111653858+hajersatar@users.noreply.github.com>
2022-08-21 01:32:29 +0300

    Update logic-pass2.py

--- logic-pass2.py
+++ logic-pass2.py
@@ -1,7 +1,6 @@
 # First, we will take the input:
-# we will take 1 as a lower value
+
 lower_value = int(input("Please, Enter the Lowest Range Value: "))
-# we will take 100 as a upper value
 upper_value = int(input("Please, Enter the Upper Range Value: "))
 
 print("The Prime Numbers in the range are: ")
